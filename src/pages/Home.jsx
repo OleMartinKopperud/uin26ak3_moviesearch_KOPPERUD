8f063916eb5c28e534047f3a6c623a43780a8d5d
Ole Martin Hansen Kopperud <olemha@hiof.no>
2026-02-27 14:00:35 +0100

    Startet på Arbeidskrav 3
    Jobbet i time med begynnelsen av arbeidskravet

---
+++ src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+
+export default function Home(){
+    const [search, setSearch] = useState()
+
+    const baseUrl = `http://www.omdbapi.com/?s=${search}&apikey=`
+    //Ikke gjør dette din dumme jævel!! 
+    const apiKey = '29a301d4'
+
+    const getMovies = async ()=>{
+        try
+        {
+            const response = await fetch(`${baseUrl}${apiKey}`)
+            const data = await response.json()
+
+            console.log(data)
+        }
+        catch(err){
+
+            console.error(err);
+        }
+
+    }
+
+    const handleChange = (e)=>{
+
+        setSearch(e.target.value)
+    }
+
+    return(
+        <main>
+            <h1>Forside</h1>
+            <form>
+                <label>
+                    Søk etter film
+                    <input type="search" placeholder="Harry Potter"onChange={handleChange}></input>
+                </label>
+            </form>
+            <button onClick={getMovies}>Søk</button>
+        </main>
+    )
+}
