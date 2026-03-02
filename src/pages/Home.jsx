0777ec04b5078c463132a02c60a4c2cfdf4dc1b9
Ole Martin Hansen Kopperud <olemha@hiof.no>
2026-03-02 14:06:10 +0100

    Fortsatt med arbeid i time

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,23 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import History from "../components/History"
 
 export default function Home(){
     const [search, setSearch] = useState()
+    const storedHistory = localStorage.getItem("search")
+    const [focused, setFocused] = useState(false)
+
+    const [history, setHistory] = useState(storedHistory ? JSON.parse(storedHistory) : [])
+
+    console.log("Denne kommer fra storage", storedHistory)
 
     const baseUrl = `http://www.omdbapi.com/?s=${search}&apikey=`
-    //Ikke gjør dette din dumme jævel!! 
-    const apiKey = '29a301d4'
+    const apiKey = import.meta.env.VITE_APP_API_KEY
 
+    useEffect(()=>{
+        localStorage.setItem("search", JSON.stringify(history))
+        
+    },[history])
+    
     const getMovies = async ()=>{
         try
         {
@@ -23,20 +34,30 @@ export default function Home(){
     }
 
     const handleChange = (e)=>{
-
         setSearch(e.target.value)
     }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        e.target.reset()
+
+        setHistory((prev) => [...prev, search])
+
+    }
+    console.log(history)
 
     return(
         <main>
             <h1>Forside</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     Søk etter film
-                    <input type="search" placeholder="Harry Potter"onChange={handleChange}></input>
+                    <input type="search" placeholder="Harry Potter"onChange={handleChange} onFocus={()=> setFocused(true)} /*onBlur={()=>setFocused(false)}*/></input>
                 </label>
+            {
+            !focused ?<History history={history} setSearch={setSearch} /> : null }
+                <button onClick={getMovies}>Søk</button>
             </form>
-            <button onClick={getMovies}>Søk</button>
         </main>
     )
 }
